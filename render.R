8032ebc62e9309dcaa9c3407cf09fa4dd6e6333f
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2020-04-01 08:15:13 +1100

    Maybe a workaround for wacky Rmarkdown rendering behaviour.

--- render.R
+++ render.R
@@ -1,3 +1,3 @@
 # Render the presentation to HTML for GitHub Pages
 
-rmarkdown::render("presentation.Rmd", output_dir = "docs", output_file = "index.html")
+rmarkdown::render("presentation.Rmd", output_dir = "docs", output_file = "index.html", quiet = TRUE)
